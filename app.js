b62e4c1ed71e93331f319e7898030fc28ffa9b36
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-03-09 22:08:16 +0530

    changes

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const App = () => {
-  return <h1>Hello</h1>;
-};
-
 const rootElement = document.getElementById("root");
 
 const root = createRoot(rootElement);
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    // this.countAdd = this.countAdd.bind(this);
+    // this.countSub = this.countSub.bind(this);
+  }
+  countAdd = () => {
+    this.setState((curState, propState) => {
+      return { count: curState.count + propState.diff };
+    });
+  };
+  countSub = () => {
+    this.setState((curState, propState) => {
+      return { count: curState.count - propState.diff };
+    });
+  };
+  render() {
+    return (
+      <>
+        <h1>Hello. from Counter</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.countAdd}>+{this.props.diff}</button>{" "}
+        <span></span>
+        <button onClick={this.countSub}>-{this.props.diff}</button>
+      </>
+    );
+  }
+}
+class App extends React.Component {
+  render() {
+    return <Counter diff={5} />;
+  }
+}
+
 root.render(<App />);
