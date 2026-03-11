0f57c6db1d8c86575f654835e39c8952ac3ceac9
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-03-11 20:11:35 +0530

    changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ class Counter extends React.Component {
     };
     // this.countAdd = this.countAdd.bind(this);
     // this.countSub = this.countSub.bind(this);
-    // this is not needed when we we use arrow functions
+    // this is not needed when we we use arrow functions fuighjkndigyubhjknmsdvluy
   }
   countAdd = () => {
     this.setState((curState, propState) => {
